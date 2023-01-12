523b18b4096d04c3af1a555a5f6541e0135eb2a0
Parker Miller <parkermiller890@gmail.com>
2023-01-11 22:11:11 -0600

    1 issue in server code. Soon to be fixed.

---
+++ Utilities Bot/server_code.py
@@ -0,0 +1,16 @@
+#Use link from Webview and sync to hosting software of your choice.
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+  return "Hello! Utilities Testing is operational."
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def server_code():
+  t = Thread(target=run)
+  t.start()
