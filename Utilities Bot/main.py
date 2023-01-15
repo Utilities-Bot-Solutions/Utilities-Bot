cb41201a2b3772f38c1f6866e01674c6a3f13833
Parker Miller <parkermiller890@gmail.com>
2023-01-14 23:10:45 -0600

    Added Music Support. Does not work. Still in works

--- Utilities Bot/main.py
+++ Utilities Bot/main.py
@@ -2,10 +2,11 @@ from discord import ChannelType
 import nextcord
 from nextcord.ext import commands
 from server_code import server_code
+import wavelink
 
 intents = nextcord.Intents.default()
 intents = nextcord.Intents().all()
-bot = commands.Bot(command_prefix="g.", intents=intents)
+bot = commands.Bot(command_prefix="u.", intents=intents)
 
 client = nextcord.Client(activity=nextcord.Game(name='Utilities Moderation Services'))
 
@@ -49,11 +50,11 @@ async def unban(interaction: nextcord.Interaction, user: nextcord.User, reason: 
   if not interaction.user.guild_permissions.ban_members:
     await interaction.response.send_message("You are not authorized to run this command.")
   else:
-      await interaction.guild.unban(nextcord.Object(id=user.id), reason=reason)
-      if logging is True:
-         log_channel = bot.get_channel(logschannel)
-         await log_channel.send(f"{user.mention} was unbanned by {interaction.user.mention} for {reason}")
-         await int.unban(reason=reason)
+    await interaction.guild.unban(nextcord.Object(id=user.id), reason=reason)
+    if logging is True:
+       log_channel = bot.get_channel(logschannel)
+       await log_channel.send(f"{user.mention} was unbanned by {interaction.user.mention} for {reason}")
+       await int.unban(reason=reason)
 
 @bot.slash_command()
 async def timeout(interaction: nextcord.Interaction, user: nextcord.User, reason: str):
@@ -66,10 +67,55 @@ async def timeout(interaction: nextcord.Interaction, user: nextcord.User, reason
          await log_channel.send(f"{user.mention} was put in timeout by {interaction.user.mention} for {reason}")
          await user.timeout(reason=reason)
 
+@bot.event
+async def joined(ctx, *, member: nextcord.Member):
+  await ctx.send(f'{member} joined on {member.joined_at}')
+
 #Note: When using this command, don't abuse it.
 @bot.slash_command()
 async def test(ctx, arg):
   await ctx.send(arg)
+
+@bot.event
+async def on_wavelink_track_end(player:wavelink.Player, track: wavelink.Track, reason):
+    ctx = player.ctx
+    vc : player = ctx.voice_client
+    
+    if vc.loop:
+        return await vc.play(track)
+    next_song= vc.queue.get()
+    await vc.play(next_song)
+    await ctx.send(f"Playing: {next_song.title}")
+@bot.slash_command()
+async def play(ctx: commands.Context,*, search: wavelink.YouTubeTrack):
+    if not ctx.voice_client:
+        vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+    elif not ctx.author.voice:
+        return await ctx.send("Please join a VC.")
+    elif not ctx.author.voice != ctx.me.voice:
+        return await ctx.send("Please be in the same VC.")
+    else:
+        vc : wavelink.Player= ctx.voice_client
+    if vc.queue.is_empty:
+        await vc.play(search)
+        await ctx.send(f"Playing: {search.title}")
+    else:
+        await vc.queue.put_wait(search)
+        await ctx.send(f"Added {search.title} to the queue.")
+    vc.ctx = ctx
+    setattr(vc,"loop",False)
+@bot.slash_command()
+async def queue(ctx: commands.Context):
+    if not ctx.voice_client:
+        return await ctx.send("Please let me join a VC.")
+    elif not ctx.author.voice:
+        return await ctx.send("Please join a VC.")
+    elif not ctx.author.voice != ctx.me.voice:
+        return await ctx.send("Please be in the same VC.")
+    else:
+        vc: wavelink.Player = ctx.voice_client
+    if vc.queue.is_empty:
+        return await ctx.send("Queue is empty.")
                               
 server_code()
-bot.run('TOKEN_ID')
+bot.run('MTA1MzQ4NTA2Mjc3MzAzNTAzOA.GF1enE.-GjtkS_b_Hdc23wIROnTNaeErsqppiuRWByKe4')
